03a621bb87f5e317ef910a3e615cf74da3775963
tamborasystem <hemanth.kv@tamborasystems.com>
2024-08-30 17:02:02 +0530

    Update model_snssai.go
    Code changes for NID according to 3gpp release 16

--- models/model_snssai.go
+++ models/model_snssai.go
@@ -16,4 +16,5 @@ package models
 type Snssai struct {
 	Sst int32  `json:"sst" yaml:"sst" bson:"sst" mapstructure:"Sst"`
 	Sd  string `json:"sd,omitempty" yaml:"sd" bson:"sd" mapstructure:"Sd"`
+	Nid string `json:"nid,omitempty" yaml:"nid" bson:"nid" mapstructure:"nid"`
 }
